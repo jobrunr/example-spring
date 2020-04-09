652834bad7a300e3080ba3c1d05c0a08081c6816
Ronald Dehuysser <ronald.dehuysser@codabox.com>
2020-04-10 01:47:14 +0200

    Cleanup

--- core/src/main/java/org/jobrunr/examples/JobRunrStorageConfiguration.java
+++ core/src/main/java/org/jobrunr/examples/JobRunrStorageConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.sqlite.SQLiteDataSource;
 
+import javax.sql.DataSource;
 import java.nio.file.Paths;
 
 @Configuration
@@ -22,12 +23,17 @@ public class JobRunrStorageConfiguration {
     }
 
     @Bean
-    public StorageProvider storageProvider(JobMapper jobMapper) {
-        final SQLiteDataSource dataSource = new SQLiteDataSource();
-        dataSource.setUrl("jdbc:sqlite:" + Paths.get(System.getProperty("java.io.tmpdir"), "jobrunr.db"));
+    public StorageProvider storageProvider(DataSource dataSource, JobMapper jobMapper) {
         final SqLiteStorageProvider sqLiteStorageProvider = new SqLiteStorageProvider(dataSource);
         sqLiteStorageProvider.setJobMapper(jobMapper);
         return sqLiteStorageProvider;
+    }
+
+    @Bean
+    public SQLiteDataSource dataSource() {
+        final SQLiteDataSource dataSource = new SQLiteDataSource();
+        dataSource.setUrl("jdbc:sqlite:" + Paths.get(System.getProperty("java.io.tmpdir"), "jobrunr.db"));
+        return dataSource;
     }
 
     @Bean
--- webapp/src/main/java/org/jobrunr/examples/webapp/WebApplication.java
+++ webapp/src/main/java/org/jobrunr/examples/webapp/WebApplication.java
@@ -1,8 +1,12 @@
 package org.jobrunr.examples.webapp;
 
 import org.jobrunr.examples.JobRunrStorageConfiguration;
+import org.jobrunr.scheduling.BackgroundJob;
+import org.jobrunr.scheduling.JobScheduler;
+import org.jobrunr.storage.StorageProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
@@ -13,4 +17,10 @@ public class WebApplication {
         SpringApplication.run(WebApplication.class, args);
     }
 
+    @Bean
+    public JobScheduler jobScheduler(StorageProvider storageProvider) {
+        final JobScheduler jobScheduler = new JobScheduler(storageProvider);
+        BackgroundJob.setJobScheduler(jobScheduler);
+        return jobScheduler;
+    }
 }
