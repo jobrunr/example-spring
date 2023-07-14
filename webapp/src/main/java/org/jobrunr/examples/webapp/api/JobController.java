878593eb7eb1b0de686bec9655986fd634869b3f
Sergi Almar <sergi.almar@gmail.com>
2023-07-14 13:51:59 +0200

    Upgrade to latest Spring Boot and JobRunr versions

--- webapp/src/main/java/org/jobrunr/examples/webapp/api/JobController.java
+++ webapp/src/main/java/org/jobrunr/examples/webapp/api/JobController.java
@@ -30,51 +30,53 @@ public class JobController {
 
     @GetMapping(produces = {MediaType.TEXT_HTML_VALUE})
     public String index() {
-        return "Hello World from JobController!<br />" +
-                "You can:<br />" +
-                "- <a href=\"/jobs/simple-job\">Enqueue a simple job</a><br />" +
-                "- <a href=\"/jobs/simple-job-instance\">Enqueue a simple job using a service instance</a><br />" +
-                "- <a href=\"/jobs/schedule-simple-job\">Schedule a simple job 3 hours from now using a service instance</a><br />" +
-                "- <a href=\"/jobs/long-running-job\">Enqueue a long-running job</a><br />" +
-                "- <a href=\"/jobs/long-running-job-with-job-context\">Enqueue a long-running job using a JobContext to log progress</a><br />" +
-                "- Learn more on <a href=\"https://www.jobrunr.io/\">www.jobrunr.io</a><br />"
+        return """
+                Hello World from JobController!<br />\
+                You can:<br />\
+                - <a href="/jobs/simple-job">Enqueue a simple job</a><br />\
+                - <a href="/jobs/simple-job-instance">Enqueue a simple job using a service instance</a><br />\
+                - <a href="/jobs/schedule-simple-job">Schedule a simple job 3 hours from now using a service instance</a><br />\
+                - <a href="/jobs/long-running-job">Enqueue a long-running job</a><br />\
+                - <a href="/jobs/long-running-job-with-job-context">Enqueue a long-running job using a JobContext to log progress</a><br />\
+                - Learn more on <a href="https://www.jobrunr.io/">www.jobrunr.io</a><br />\
+                """
                 ;
     }
 
     @GetMapping(value = "/simple-job", produces = {MediaType.TEXT_PLAIN_VALUE})
-    public String simpleJob(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public String simpleJob(@RequestParam(defaultValue = "World") String name) {
         final JobId enqueuedJobId = jobScheduler.<MyService>enqueue(myService -> myService.doSimpleJob("Hello " + name));
         return "Job Enqueued: " + enqueuedJobId;
     }
 
     @GetMapping(value = "/simple-job-instance", produces = {MediaType.TEXT_PLAIN_VALUE})
-    public String simpleJobUsingInstance(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public String simpleJobUsingInstance(@RequestParam(defaultValue = "World") String name) {
         final JobId enqueuedJobId = jobScheduler.enqueue(() -> myService.doSimpleJob("Hello " + name));
         return "Job Enqueued: " + enqueuedJobId;
     }
 
     @GetMapping(value = "/schedule-simple-job", produces = {MediaType.TEXT_PLAIN_VALUE})
     public String scheduleSimpleJob(
-            @RequestParam(value = "value", defaultValue = "Hello world") String value,
-            @RequestParam(value = "when", defaultValue = "PT3H") String when) {
+            @RequestParam(defaultValue = "Hello world") String value,
+            @RequestParam(defaultValue = "PT3H") String when) {
         final JobId scheduledJobId = jobScheduler.schedule(now().plus(Duration.parse(when)), () -> myService.doSimpleJob(value));
         return "Job Scheduled: " + scheduledJobId;
     }
 
     @GetMapping(value = "/long-running-job", produces = {MediaType.TEXT_PLAIN_VALUE})
-    public String longRunningJob(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public String longRunningJob(@RequestParam(defaultValue = "World") String name) {
         final JobId enqueuedJobId = jobScheduler.<MyService>enqueue(myService -> myService.doLongRunningJob("Hello " + name));
         return "Job Enqueued: " + enqueuedJobId;
     }
 
     @GetMapping(value = "/long-running-job-with-job-context", produces = {MediaType.TEXT_PLAIN_VALUE})
-    public String longRunningJobWithJobContext(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public String longRunningJobWithJobContext(@RequestParam(defaultValue = "World") String name) {
         final JobId enqueuedJobId = jobScheduler.<MyService>enqueue(myService -> myService.doLongRunningJobWithJobContext("Hello " + name, JobContext.Null));
         return "Job Enqueued: " + enqueuedJobId;
     }
 
     @GetMapping(value = "/delete-job", produces = {MediaType.TEXT_PLAIN_VALUE})
-    public String deleteJob(@RequestParam(value = "jobId") UUID jobId) {
+    public String deleteJob(@RequestParam UUID jobId) {
         jobScheduler.delete(jobId);
         return "Job Deleted: " + jobId;
     }
